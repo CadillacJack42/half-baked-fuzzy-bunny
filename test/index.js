1e5b807dc6d9872858a33f921cc7654eb8cdb52c
James Keicher <jamesdkeicher@gmail.com>
2022-01-10 16:01:17 -0800

    5th attempt

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 
 // include jsdom for DOM use in tests on travis
 const jsdom = require('jsdom');
-import { SUPABASE_KEY, SUPABASE_URL } from '../fetch-utils.js'
+// import { SUPABASE_KEY, SUPABASE_URL } from '../fetch-utils.js'
 // const  = require('../fetch-utils.js');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM(``, {
@@ -18,5 +18,8 @@ global.URL = window.URL;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
+
+export const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
